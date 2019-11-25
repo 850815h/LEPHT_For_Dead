0020ae25b7b81ea5d13389e753ebd4cc7e10aa70
Tobias Severfelt <tobias.severfelt@hotmail.com>
2019-11-25 18:52:52 +0100

    create PlayerFactory class

---
+++ src/com/company/PlayerFactory.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PlayerFactory {
+
+    private Random skill = new Random();
+    private ArrayList<String> firstNames = new ArrayList<>();
+    private ArrayList<String> lastNames = new ArrayList<>();
+
+    private void addFirstNames() {
+        firstNames.add("Bamse");
+        firstNames.add("Tomten");
+        firstNames.add("Lennart");
+        firstNames.add("Hassan");
+        firstNames.add("Hulken");
+        firstNames.add("Emanuel");
+        firstNames.add("Leona");
+        firstNames.add("Pontus");
+        firstNames.add("Tobias");
+        firstNames.add("Mimmi");
+        firstNames.add("Fantomen");
+        firstNames.add("Catwoman");
+        firstNames.add("Pluto");
+        firstNames.add("Piff");
+        firstNames.add("Puff");
+        firstNames.add("Kalle");
+        firstNames.add("Musse");
+        firstNames.add("Ali");
+        firstNames.add("Cliff");
+        firstNames.add("Arnold");
+        firstNames.add("Zlatan");
+        firstNames.add("WonderWoman");
+        firstNames.add("Betty Boop");
+        firstNames.add("Tom");
+        firstNames.add("Jerry");
+    }
+
+    private void addLastName() {
+        lastNames.add("McRib");
+        lastNames.add("Steel");
+        lastNames.add("Diakite");
+        lastNames.add("Stallone");
+        lastNames.add("Armstrong");
+        lastNames.add("Svensson");
+        lastNames.add("Hussein");
+        lastNames.add("Strong");
+        lastNames.add("Schwarzenegger");
+        lastNames.add("Stålnacke");
+        lastNames.add("af von Knapp");
+        lastNames.add("Terminator");
+        lastNames.add("Jonsson");
+        lastNames.add("BigMac");
+        lastNames.add("Maradona");
+        lastNames.add("Köttfärs");
+        lastNames.add("Baba");
+        lastNames.add("Beerpong");
+        lastNames.add("Wordfeud");
+        lastNames.add("Candy Crush");
+        lastNames.add("Messi");
+        lastNames.add("Ronaldo");
+        lastNames.add("Ibrahimovic");
+        lastNames.add("Silverhjelm");
+        lastNames.add("Lingongrova");
+    }
+
+}
