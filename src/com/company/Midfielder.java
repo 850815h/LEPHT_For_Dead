9d7d00fdd29878057d684057b81593f6e69ed38a
Hassan <850815h@gmail.com>
2019-11-27 12:48:54 +0100

    La till Player-klassen och dess sub-klasser

---
+++ src/com/company/Midfielder.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Midfielder extends Player {
+
+    public Midfielder(String name, int speed, int dribbling, int passing, int breaking) {
+        super(name, speed, dribbling, passing, breaking);
+    }
+}
