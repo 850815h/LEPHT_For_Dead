38196107a0bb59a060c7f677b708fc6c9cfd4807
Hassan <850815h@gmail.com>
2019-12-03 13:43:37 +0100

    CleanUpCode

---
+++ src/com/company/Team.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Team {
+    /*
+    attribut: 11 x Player, namn, goalPlus/minus, losses, wins, ties, points
+     */
+
+    private String teamName;
+    private int goalPlus;
+    private int losses;
+    private int wins;
+    private int ties;
+    private int points;
+    //private boolean hasPossession = false;
+
+    //private TeamStats teamStats = new TeamStats();
+    private ArrayList<Player> team;
+
+
+    public Team(String teamName, ArrayList<Player> team) {
+        this.teamName = teamName;
+        this.team = team;
+    }
+
+    public String toString() {
+        return teamName;
+    }
+
+    public String getName() {
+        return teamName;
+    }
+
+    public ArrayList<Player> getTeam() {
+        return team;
+    }
+
+    /*
+    public boolean isHasPossession() { return hasPossession; }
+    public void setHasPossession(boolean hasPossession) { this.hasPossession = hasPossession; }
+    public TeamStats getTeamStats(){ return teamStats; }
+     */
+
+}
