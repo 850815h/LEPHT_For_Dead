3f11a1f72b4c1a134242be307e6b6ec75e8ca730
Hassan <850815h@gmail.com>
2019-12-04 11:48:35 +0100

    Chnages in FS, Player, Team

--- src/com/company/LeagueTable.java
+++ src/com/company/LeagueTable.java
@@ -4,7 +4,20 @@ import java.util.ArrayList;
 
 public class LeagueTable {
 
+    private  ArrayList<Team> league;
+
+    public LeagueTable(ArrayList<Team> teamList){
+        this.league = teamList;
+    }
+
+
+
     public ArrayList<int[]> createMatchOrder(){
         return null;
     }
+
+    public ArrayList<Team> getLeague(){
+        return league;
+    }
+
 }
