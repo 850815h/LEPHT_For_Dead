6840d0a4f1c079d603cd5a776b2feb31d790cf35
Pontus <pontus.jungeby@hotmail.com>
2019-12-04 21:13:35 +0100

    Defender

--- src/com/company/Defender.java
+++ src/com/company/Defender.java
@@ -2,7 +2,26 @@ package com.company;
 
 public class Defender extends Player {
 
-    public Defender(String name, int speed, int dribbling, int passing, int breaking, String position) {
-        super(name, speed, dribbling, passing, breaking, position);
+    public Defender(String name, String teamName, int speed, int dribblingAbility, int passingAbility, int breakingAbility, String position) {
+        super(name, speed, dribblingAbility, breakingAbility, passingAbility, position, teamName);
+        originalPosition = 1;
+        currentPosition = 1;
+    }
+
+    @Override
+    public int getAction(Team ownTeam,Team opposingTeam, int gameAction) {
+        if (currentPosition == 3){
+            if (tryToShoot(ownTeam, opposingTeam, gameAction)) {
+                return 1;
+            } else return 0;
+        }
+        int whatToDo = random.nextInt(3);
+        if (whatToDo <= 1) {
+            tryToPass(ownTeam, opposingTeam);
+            return 0;
+        } else {
+            tryToDribble(ownTeam, opposingTeam, gameAction);
+        }
+        return 0;
     }
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -3,29 +3,43 @@ package com.company;
 import java.util.Random;
 
 public abstract class Player {
-
-    protected Random random = new Random();
+    Random random = new Random();
     private String name;
+    private String teamName;
     protected int currentPosition;
     protected int originalPosition;
     private boolean hasTheBall = false;
     protected String position;
     private int speed;
-    private int passing;
-    private int dribbling;
-    private int breaking;
-    protected int saving;
+    private int passingAbility;
+    private int dribblingAbility;
+    private int breakingAbility;
+    protected int savingAbility;
     private PlayerStats playerStats;
 
-    public Player(String name, String teamName, String position, int speed, int passing, int dribbling, int breaking, int saving) {
+    public Player(String name, String teamName, String position, int speed, int passingAbility, int dribblingAbility, int breakingAbility, int savingAbility) {
         this.name = name;
+        this.teamName = teamName;
         this.position = position;
         this.speed = speed;
-        this.passing = passing;
-        this.dribbling = dribbling;
-        this.breaking = breaking;
-        this.saving = saving;
+        this.passingAbility = passingAbility;
+        this.dribblingAbility = dribblingAbility;
+        this.breakingAbility = breakingAbility;
+        this.savingAbility = savingAbility;
         this.playerStats = new PlayerStats(this);
     }
 
+    public abstract int getAction(Team opposingTeam, Team ownTeam, int gameAction);
+
+    public void tryToPass() {
+
+    }
+
+    public void tryToDribble() {
+
+    }
+
+    public void tryToShoot() {
+
+    }
 }
