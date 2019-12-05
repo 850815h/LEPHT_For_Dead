c561ab9aa3e9f05c7f4c1fd0fe856e6552fa40db
Emanuel Harling <emanuelharling@hotmail.com>
2019-12-05 18:00:43 +0100

    Final changes, bugfixes and so on

--- src/com/company/Midfielder.java
+++ src/com/company/Midfielder.java
@@ -9,7 +9,8 @@ public class Midfielder extends Player {
     @Override
     public int getAction(Team ownTeam,Team opposingTeam, int gameAction) {
         if (currentPosition == 3){
-            if (tryToShoot(ownTeam, opposingTeam, gameAction)) { return 1;
+            if (tryToShoot(ownTeam, opposingTeam, gameAction)) {
+                return 1;
             } else return 0;
         }
         int whatToDo = random.nextInt(2);
